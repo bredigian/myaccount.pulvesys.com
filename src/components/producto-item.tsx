4eba5e29850a74d313215abaf13c5432e16dc213
Gianluca Bredice <bredicegian@gmail.com>
2025-01-24 12:37:20 -0300

    change: Producto item UI

--- src/components/producto-item.tsx
+++ src/components/producto-item.tsx
@@ -5,8 +5,9 @@ import {
   DeleteProductoDialog,
 } from './productos-dialog';
 import { Card, CardContent } from './ui/card';
+import { Producto, UNIDAD } from '@/types/productos.types';
 
-import { Producto } from '@/types/productos.types';
+import { Badge } from './ui/badge';
 import { UUID } from 'crypto';
 
 interface Props {
@@ -14,15 +15,25 @@ interface Props {
 }
 
 export default function ProductoItem({ data }: Props) {
+  const parsedUnidad = data.unidad
+    .charAt(0)
+    .concat(data.unidad.substring(1).toLowerCase());
+
   return (
-    <li className='flex items-center justify-between'>
+    <li className='flex items-start justify-between'>
       <Card className='hover:bg-sidebar-accent w-full duration-200 ease-in-out'>
-        <CardContent className='flex items-center justify-between pt-6'>
-          <div className='space-y-2'>
-            <span className='text-base'>{data.nombre}</span>
-            <p className='text-sm'>
-              {data.cantidad} {data.unidad}
-            </p>
+        <CardContent className='flex items-start justify-between pt-6'>
+          <div className='space-y-1'>
+            <h3 className='text-base font-semibold'>{data.nombre}</h3>
+            <div className='flex items-center gap-2'>
+              <p className='text-sm text-muted-foreground'>
+                20
+                {data.unidad === UNIDAD.GRAMOS.toUpperCase()
+                  ? data.unidad.charAt(0).toLowerCase()
+                  : data.unidad.charAt(0)}{' '}
+              </p>
+              <Badge variant='secondary'>{parsedUnidad}</Badge>
+            </div>
           </div>
           <aside className='producto-settings space-x-4'>
             <AddOrEditProductoDialog isEdit data={data} />
