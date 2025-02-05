3f6bca5d400bb8f5dac45e9f987a411494aabace
Gianluca Bredice <bredicegian@gmail.com>
2025-02-05 15:14:03 -0300

    feature: responsive design on logout dialog

--- src/components/logout-dialog.tsx
+++ src/components/logout-dialog.tsx
@@ -1,4 +1,14 @@
 import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './ui/dialog';
+import {
   Drawer,
   DrawerClose,
   DrawerContent,
@@ -12,19 +22,29 @@ import {
 import { Button } from './ui/button';
 import Cookies from 'js-cookie';
 import { LogOut } from 'lucide-react';
+import { ReloadIcon } from '@radix-ui/react-icons';
+import { cn } from '@/lib/utils';
 import { signout } from '@/services/auth.service';
 import { toast } from 'sonner';
 import { useDialog } from '@/hooks/use-dialog';
+import { useIsMobile } from '@/hooks/use-mobile';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import { usuarioStore } from '@/store/usuario.store';
+
+type State = 'pending' | 'success' | 'error';
 
 export default function LogoutDialog() {
   const { open, setOpen } = useDialog();
   const { push } = useRouter();
   const { clearUserdata } = usuarioStore();
 
+  const [state, setState] = useState<undefined | State>(undefined);
+
   const handleLogout = async () => {
     try {
+      setState('pending');
+
       const access_token = Cookies.get('access_token');
       if (!access_token) {
         push(`/?session=${access_token}&expired=true`);
@@ -38,15 +58,22 @@ export default function LogoutDialog() {
 
       clearUserdata();
 
-      toast.success('La sesión ha sido cerrada');
-      push('/');
+      setState('success');
+
+      setTimeout(() => {
+        toast.success('La sesión ha sido cerrada');
+        push('/');
+      }, 1000);
     } catch (e) {
+      setState('error');
       if (e instanceof Error)
         toast.error(e.message, { position: 'top-center' });
     }
   };
 
-  return (
+  const isMobile = useIsMobile();
+
+  return isMobile ? (
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerTrigger asChild>
         <Button variant={'destructive'} size={'icon'}>
@@ -59,8 +86,29 @@ export default function LogoutDialog() {
           <DrawerDescription>Se cerrará la sesión</DrawerDescription>
         </DrawerHeader>
         <DrawerFooter className=''>
-          <Button variant={'destructive'} onClick={handleLogout}>
-            Salir
+          <Button
+            disabled={state === 'pending' || state === 'success'}
+            variant={'destructive'}
+            onClick={handleLogout}
+            className={cn(
+              'disabled:opacity-100',
+              state === 'success' && '!bg-green-700',
+            )}
+          >
+            {state === undefined ? (
+              <>Salir</>
+            ) : state === 'pending' ? (
+              <>
+                Saliendo <ReloadIcon className='animate-spin' />
+              </>
+            ) : (
+              state === 'success' && (
+                <>
+                  Sesión cerrada. Redirigiendo{' '}
+                  <ReloadIcon className='animate-spin' />
+                </>
+              )
+            )}
           </Button>
           <DrawerClose asChild>
             <Button variant={'outline'}>Cancelar</Button>
@@ -68,5 +116,48 @@ export default function LogoutDialog() {
         </DrawerFooter>
       </DrawerContent>
     </Drawer>
+  ) : (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button variant={'destructive'} size={'icon'}>
+          <LogOut />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>¿Estás seguro?</DialogTitle>
+          <DialogDescription>Se cerrará la sesión</DialogDescription>
+        </DialogHeader>
+        <DialogFooter className=''>
+          <Button
+            disabled={state === 'pending' || state === 'success'}
+            variant={'destructive'}
+            onClick={handleLogout}
+            className={cn(
+              'disabled:opacity-100',
+              state === 'success' && '!bg-green-700',
+            )}
+          >
+            {state === undefined ? (
+              <>Salir</>
+            ) : state === 'pending' ? (
+              <>
+                Saliendo <ReloadIcon className='animate-spin' />
+              </>
+            ) : (
+              state === 'success' && (
+                <>
+                  Sesión cerrada. Redirigiendo{' '}
+                  <ReloadIcon className='animate-spin' />
+                </>
+              )
+            )}
+          </Button>
+          <DialogClose asChild>
+            <Button variant={'outline'}>Cancelar</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
